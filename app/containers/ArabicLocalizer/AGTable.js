2b59b6343b8980cb9e2f2fb715906281a6fd3898
menna-ussef <menna.u.abdelfattah@gmail.com>
2019-10-24 13:23:44 +0200

    init ag-grid @ dandelion

---
+++ app/containers/ArabicLocalizer/AGTable.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { PapperBlock } from 'dan-components';
+import AgTable from '../../components/AgTableSS';
+
+class AGTable extends React.Component {
+  render() {
+    const title = 'Dandelion Pro. Blank Page';
+    const description = 'Dandelion Pro';
+    return (
+      <div>
+        <Helmet>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="twitter:title" content={title} />
+          <meta property="twitter:description" content={description} />
+        </Helmet>
+        <PapperBlock title="Ag-Table" desc="Server Side Data Table">
+          <AgTable/>
+        </PapperBlock>
+      </div>
+    );
+  }
+}
+
+export default AGTable;
